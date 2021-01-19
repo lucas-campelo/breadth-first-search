cdee14994202bace6c8f53959f86737b5ff54d26
Lucas Campelo <lucas_campelo3009@hotmail.com>
2021-01-19 14:22:46 -0300

    Changed graph example

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from graph import Node
 from algorithm import bfs
 
 g = [
-    Node(label='A' ,dist=maxsize, adj=[1, 4]),
-    Node(label='B' ,dist=maxsize, adj=[2, 4]),
-    Node(label='C' ,dist=maxsize, adj=[1, 3]),
-    Node(label='D' ,dist=maxsize, adj=[2, 4, 5]),
-    Node(label='E' ,dist=maxsize, adj=[0, 1, 3]),
-    Node(label='F' ,dist=maxsize, adj=[3])
+    Node(label='A' ,dist=maxsize, adj=[1, 3]),
+    Node(label='B' ,dist=maxsize, adj=[0, 2, 5]),
+    Node(label='C' ,dist=maxsize, adj=[1, 6]),
+    Node(label='D' ,dist=maxsize, adj=[0, 5, 4]),
+    Node(label='E' ,dist=maxsize, adj=[3, 5, 6]),
+    Node(label='F' ,dist=maxsize, adj=[1, 3, 4]),
+    Node(label='G' ,dist=maxsize, adj=[2, 1, 4, 7]),
+    Node(label='H' ,dist=maxsize, adj=[6])
 ]
 
 if __name__ == '__main__':
     tree = bfs(graph=g, start=g[0])
+    # print(tree)
     for node in tree:
         print(node)
